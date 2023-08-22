dab4d4ddc5c8bc3c4a78b12750f28984b8208a77
yadavneerajdev <connectneerajyadav@gmail.com>
2023-08-22 10:30:33 +0530

    4th excersize modified with 'and' in the array

--- HTML/Excersize4/script.js
+++ HTML/Excersize4/script.js
@@ -1,38 +1,44 @@
 const user = {
-    name: "Piyush Sharma",
-    designation: "Senior Software Engineer",
-    company: "Infuse Consulting",
-    hobbies: ["Reading", "Listening to music", "Collecting stamps"],
-  };
-  
-  let uname = document.getElementById("name");
-  let designation = document.getElementById("designation");
-  let company = document.getElementById("company");
-  let hobby = document.getElementById("hobby");
-  
-  /**
-   * Use destructuting to log the following
-   */
-  
-  const printUserProfile = () => {
-    // Piyush Sharma is a Senior Software Engineer at Infuse Consulting. He likes Reading, Listening to music and Collecting stamps
-  
-    // Print on front end
-  
-    uname.innerHTML = user.name;
-    designation.innerHTML = user.designation;
-    company.innerHTML = "<span>" + user.company + "</span>";
-  
-    hobby.innerHTML = "Hobbies : </br>" + user.hobbies.join();
-  
-    // For printing in console
-  
-    console.log(
-      `${user.name} is a ${user.designation} at ${
-        user.company
-      }. He likes ${user.hobbies.join()}`
-    );
-  };
-  
-  printUserProfile();
-  
+  name: "Piyush Sharma",
+  designation: "Senior Software Engineer",
+  company: "Infuse Consulting",
+  hobbies: ["Reading", "Listening to music", "Collecting stamps"],
+};
+
+let uname = document.getElementById("name");
+let designation = document.getElementById("designation");
+let company = document.getElementById("company");
+let hob = document.getElementById("hobby");
+
+/**
+ * Use destructuting to log the following
+ */
+
+const printUserProfile = () => {
+  // Piyush Sharma is a Senior Software Engineer at Infuse Consulting. He likes Reading, Listening to music and Collecting stamps
+
+  // Print on front end
+
+  uname.innerHTML = user.name;
+  designation.innerHTML = user.designation;
+  company.innerHTML = "<span>" + user.company + "</span>";
+  let hobby = user.hobbies;
+
+  hob.innerHTML =
+    "<span>Hobbies</span> : </br>" +
+    `${user.name} is a ${user.designation} at ${user.company}. He likes ${hobby
+      .slice(0, hobby.length - 1)
+      .join(",")} and ${hobby[hobby.length - 1]}`;
+
+  // For printing in console
+
+  user;
+
+  console.log(
+    `${user.name} is a ${user.designation} at ${user.company}. He likes ${hobby
+      .slice(0, hobby.length - 1)
+      .join(",")} and ${hobby[hobby.length - 1]}`
+  );
+};
+
+printUserProfile();
