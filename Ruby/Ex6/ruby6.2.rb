baa7e9389100bb72471425b9cfbdd7a28eaa041e
yadavneerajdev <connectneerajyadav@gmail.com>
2023-08-22 15:25:58 +0530

    6th exercise unit test added

--- Ruby/Ex6/ruby6.2.rb
+++
@@ -1,8 +0,0 @@
-require 'base64'
-
-original_string = 'the string that will be base encoded'
-encoded_string = Base64.encode64(original_string)
-
-File.write('encoded_string.txt', encoded_string)
-
-puts "Encoded string saved to 'encoded_string.txt'"
--- Ruby/Ex6/ruby6.3.rb
+++
@@ -1,8 +0,0 @@
-# write_file.rb
-
-encoded_string = File.read('encoded_string.txt')
-
-file_name = 'Neeraj_yadav.txt'
-File.write(file_name, encoded_string)
-
-puts "Encoded string written to #{file_name}"
