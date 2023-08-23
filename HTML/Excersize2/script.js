79c1164173fd4cb1b84528508179a8ca87412a6e
yadavneerajdev <connectneerajyadav@gmail.com>
2023-08-23 11:37:37 +0530

    Added const to Excersize 2 - PR request

--- HTML/Excersize2/script.js
+++ HTML/Excersize2/script.js
@@ -1,22 +1,18 @@
-let clock = document.getElementById("clock");
-let daytime = document.getElementById("daytime");
+const clock = document.getElementById("clock");
+const daytime = document.getElementById("daytime");
 
+function getTime() {
+  const date = new Date();
+  const hour = date.getHours();
+  const min = date.getMinutes();
+  const sec = date.getSeconds();
+  clock.innerHTML = hour + " : " + min + " : " + sec + " ";
 
-function getTime(){
-    let date = new Date();
-    let hour = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
-    clock.innerHTML = hour + " : " + min + " : " + sec + " ";
-
-    if(hour < 12 && hour >= 0){
-        daytime.innerHTML = " AM";
-    }else{
-        daytime.innerHTML = " PM";
-    }
-
-
+  if (hour < 12 && hour >= 0) {
+    daytime.innerHTML = " AM";
+  } else {
+    daytime.innerHTML = " PM";
+  }
 }
- 
 
 setInterval(getTime, 1000);
