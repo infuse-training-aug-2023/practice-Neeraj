57c6244a54e0a9a8d617c3477aed868e557279db
yadavneerajdev <connectneerajyadav@gmail.com>
2023-08-23 15:39:48 +0530

    PR resolved for exercise 4

--- Ruby/Ex4/unit_test4.rb
+++ Ruby/Ex4/unit_test4.rb
@@ -7,7 +7,22 @@ class TestSportsManipulator < Test::Unit::TestCase
   end
 
   def test_skip_sports
-    expected_result = ['Hockey', 'Ludo', 'Rummy Circle', 'Snake and ledder']
+    expected_result = ['2:Hockey', '3:Ludo', '4:Rummy Circle', '5:Snake and ledder']
     assert_equal expected_result, @manipulator.skip_sports(2)
+  end
+
+  def test_skip_sports_with_zero_count
+    expected_result = ['0:Cricket', '1:Football', '2:Hockey', '3:Ludo', '4:Rummy Circle', '5:Snake and ledder']
+    assert_equal expected_result, @manipulator.skip_sports(0)
+  end
+
+  def test_skip_sports_with_negative_count
+    expected_result = ['0:Cricket', '1:Football', '2:Hockey', '3:Ludo', '4:Rummy Circle', '5:Snake and ledder']
+    assert_equal expected_result, @manipulator.skip_sports(-2)
+  end
+
+  def test_skip_sports_with_count_beyond_length
+    expected_result = []
+    assert_equal expected_result, @manipulator.skip_sports(10)
   end
 end
