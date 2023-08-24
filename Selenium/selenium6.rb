16139ec25e9e0dcb6823905bd62a88bc251367cd
yadavneerajdev <connectneerajyadav@gmail.com>
2023-08-24 09:47:15 +0530

    PR changes for Excercise 6 done

--- Selenium/selenium6.rb
+++ Selenium/selenium6.rb
@@ -1,16 +1,17 @@
 require 'selenium-webdriver'
 
 # Open Chrome Browser
-
 Selenium::WebDriver::Chrome::Service.driver_path = 'C:\Users\neera.yadav\Documents\BrowserDrivers\chromedriver-win64\chromedriver-win64\chromedriver.exe'
 driver = Selenium::WebDriver.for :chrome
 
 driver.get 'https://letcode.in/forms'
 
-dropdown = driver.find_element(:xpath , '/html/body/app-root/app-forms/section[1]/div/div/div[1]/div/div/form/div[5]/div[2]/div/div/div/select')
+dropdown_element = driver.find_element(:xpath, '/html/body/app-root/app-forms/section[1]/div/div/div[1]/div/div/form/div[5]/div[2]/div/div/div/select')
 
-options = dropdown.find_elements(tag_name: 'option')
+dropdown = Selenium::WebDriver::Support::Select.new(dropdown_element)
 
+# Get all options and print their text
+options = dropdown.options
 options.each do |option|
   puts option.text
 end
