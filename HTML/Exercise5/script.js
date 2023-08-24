fea83c4aad82d1029950bacf5eec4e66f61a74e3
yadavneerajdev <connectneerajyadav@gmail.com>
2023-08-24 12:25:35 +0530

    Made PR changes to exercise 5

--- HTML/Exercise5/script.js
+++ HTML/Exercise5/script.js
@@ -1,134 +1,94 @@
 const search = document.querySelector(".query");
 const container = document.querySelector(".container");
 const select = document.getElementById("update");
-const rangep = document.getElementById("range");
-const update = document.getElementById("update");
 const load = document.getElementById("load");
 
-update.addEventListener("change", (e) => sortData(e.target.value));
+const url = "https://www.omdbapi.com/?apikey=d5a4ba2c&s=";
+
+select.addEventListener("change", () => sortData(select.value));
 
 let data = [];
-
-let pageCount = 1;
-
 let years = []; // all fetched years from response
 
-let btn = document.getElementById("unique");
 let loader = false;
 
-btn.addEventListener("click", fetchData());
+document.getElementById("unique").addEventListener("click", fetchData);
 
 async function fetchData() {
   container.innerHTML = "";
-  load.innerText = "Loading please wait...";
-  let fragment = document.createDocumentFragment();
-  let fragment2 = document.createDocumentFragment();
+  load.textContent = "Loading, please wait...";
   years = [];
 
   if (search.value === "") return;
-  for (let i = 1; i < 100; i++) {
-    data = await (
-      await fetch(
-        `https://www.omdbapi.com/?apikey=d5a4ba2c&s=${search.value}&page=${i}`
-      )
-    ).json();
-    if (data.Search != undefined && years.length <= 1000) {
-      years.push(...data.Search);
-      console.log(years);
-    } else {
-      container.innerHTML = "";
 
-      years
-        .sort((elem1, elem2) => {
-          return elem1.Year - elem2.Year;
-        })
-        .forEach((item) => {
-          const card = document.createElement("div");
-          card.className = "card";
-          card.innerHTML = `
-      <div class="image">
-      <img src="${item.Poster}" />
-      </div>
-      <div class="desc">
-        <h1 class="title">${item.Title}</h1>
-        <p class="year"><span>${item.Year}</span> <span>${item.Type}</span></p>
-      </div>
-    `;
+  try {
+    for (let i = 1; i < 100; i++) {
+      const response = await fetch(url + `${search.value}&page=${i}`);
 
-          fragment.appendChild(card);
-        });
-
-      const distinctArray = years.reduce((uniqueValues, value) => {
-        if (!uniqueValues.includes(value)) {
-          uniqueValues.push(value);
+      if (response.ok) {
+        const responseData = await response.json();
+        if (responseData.Search !== undefined && years.length <= 1000) {
+          years.push(...responseData.Search);
+        } else {
+          renderData();
+          load.innerHTML = "";
+          return;
         }
-        return uniqueValues;
-      }, []);
-
-      distinctArray.forEach((item) => {
-        select.innerHTML = '<option value="all">All</option>';
-        const option = document.createElement("option");
-        option.innerText = item.Year;
-        option.setAttribute("value", item.Year);
-
-        fragment2.appendChild(option);
-      });
-
-      container.appendChild(fragment);
-      select.appendChild(fragment2);
-
-      console.log("exit");
-      load.innerHTML = "";
-
-      return;
+      } else {
+        console.error(`Request failed with status ${response.status}`);
+      }
     }
+
+    renderData();
+  } catch (error) {
+    console.error("An error occurred:", error);
   }
 
-  console.log(years);
   load.innerHTML = "";
 }
 
-function sortData(e) {
-  let fragment = document.createDocumentFragment();
-  const fragment2 = document.createDocumentFragment();
-
+function renderData() {
   container.innerHTML = "";
+  select.innerHTML = '<option value="all">All</option>';
 
-  console.log(e);
-
-  years
-    .sort((elem1, elem2) => {
-      return elem1.Year - elem2.Year;
-    })
-    .forEach((item) => {
-      if (item.Year === e || e === "all") {
-        const card = document.createElement("div");
-        card.className = "card";
-        card.innerHTML = `
-      <div class="image">
-      <img src="${item.Poster}" />
-      </div>
-      <div class="desc">
-        <h1 class="title">${item.Title}</h1>
-        <p class="year"><span>${item.Year}</span> <span>${item.Type}</span></p>
-      </div>
-    `;
-
-        fragment.appendChild(card);
-      }
+  const distinctYears = [...new Set(years.map((item) => item.Year))];
+  distinctYears
+    .sort((a, b) => a - b)
+    .forEach((year) => {
+      const option = document.createElement("option");
+      option.textContent = year;
+      option.value = year;
+      select.appendChild(option);
     });
 
-  container.appendChild(fragment);
-  rangep.innerText = " Results for : " + e;
+  sortData(select.value);
 }
 
-function onSearch(e) {
-  const distinctArray = years.reduce((uniqueValues, value) => {
-    if (!uniqueValues.includes(value)) {
-      uniqueValues.push(value);
-    }
-    return uniqueValues;
-  }, []);
+function sortData(selectedYear) {
+  container.innerHTML = "";
+  const filteredYears = years.filter(
+    (item) => selectedYear === "all" || item.Year === selectedYear
+  );
 
-  console.log(distinctArray);
+  filteredYears
+    .sort((a, b) => a.Year - b.Year)
+    .forEach((item) => {
+      const card = document.createElement("div");
+      card.className = "card";
+      card.innerHTML = `
+        <div class="image">
+          <img src="${item.Poster}" />
+        </div>
+        <div class="desc">
+          <h1 class="title">${item.Title}</h1>
+          <p class="year"><span>${item.Year}</span> <span>${item.Type}</span></p>
+        </div>
+      `;
+      container.appendChild(card);
+    });
+
+  document.getElementById("range").textContent =
+    selectedYear === "all"
+      ? " Results for : all"
+      : ` Results for : ${selectedYear}`;
 }
