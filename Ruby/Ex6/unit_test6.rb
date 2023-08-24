5c3376724a1e86aa22145cbd45ab0bb18ce08036
yadavneerajdev <connectneerajyadav@gmail.com>
2023-08-24 09:14:04 +0530

    6th PR further changes made

--- Ruby/Ex6/unit_test6.rb
+++ Ruby/Ex6/unit_test6.rb
@@ -7,36 +7,18 @@ class TestBase64Encoder < Test::Unit::TestCase
     @output_file = 'Neeraj_yadav.txt'
     @base64_encoder = Base64Encoder.new(@input_file, @output_file)
   end
-
-  def teardown
-    File.delete(@input_file) if File.exist?(@input_file)
-    File.delete(@output_file) if File.exist?(@output_file)
-  end
-
-  def test_process_and_write_encoded_content_with_valid_input
+  
+  def test_encode_and_replace_base64_string
     content = 'Originally... [ "the string that will be base encoded" ]. English law in 1852.'
-    File.write(@input_file, content)
-
-    result = @base64_encoder.process_and_write_encoded_content
-
-    expected_encoded_content = 'Originally... [ "dGhlIHN0cmluZyB0aGF0IHdpbGwgYmUgYmFzZSBlbmNvZGVk" ]. English law in 1852.'
-    assert_equal "Encoded content saved to #{@output_file}", result
-    assert_equal expected_encoded_content, File.read(@output_file).strip
+    updated_content = @base64_encoder.encode_and_replace_base64_string(content)
+    assert_equal 'Originally... [ "dGhlIHN0cmluZyB0aGF0IHdpbGwgYmUgYmFzZSBlbmNvZGVk" ]. English law in 1852.', updated_content
   end
 
-  def test_process_and_write_encoded_content_with_invalid_input
-    File.write(@input_file, 'Invalid input content')
-
-    result = @base64_encoder.process_and_write_encoded_content
-
-    assert_match(/^An error occurred: /, result)
-    assert_false File.exist?(@output_file)
-  end
-
-  def test_process_and_write_encoded_content_with_missing_input_file
-    result = @base64_encoder.process_and_write_encoded_content
-
-    assert_match(/^An error occurred: /, result)
-    assert_false File.exist?(@output_file)
+  def test_write_to_output_file
+    content = 'Originally, John Doe was a sham name used to indicate any plaintiff in an action of ejectment (a legal action to regain property) in civil court [ "the string that will be base encoded" ]. Richard Roe was the counterpart, to indicate the defendant. These fake names were used in delicate legal matters, a practice that was abolished in English law in 1852.'
+    updated_content = @base64_encoder.encode_and_replace_base64_string(content)
+    @base64_encoder.write_to_output_file(updated_content)
+    output_content = File.read(@output_file)
+    assert_equal updated_content, output_content.strip
   end
 end
